2c7b4770c43e7c1c02cfa6495167190d1512c86b
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-27 14:30:57 +0530

    create new input and built btn in typescript and create new item variable thats assign input value and show below

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first_project';
+  item = '';
+  cart: string[] = [];
+
+  add(e: any) {
+    this.item = e.target.value;
+  }
+
+  addcart() {
+    this.cart.push(this.item);
+  }
 }
