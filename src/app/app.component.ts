1466962501de4542d3b6cb30329ff8ec61836606
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-29 09:05:14 +0530

    Routing setup with add new Nav Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from '../components/data-binding/data-binding.component';
 import { StructuralDirectivesComponent } from '../components/structural-directives/structural-directives.component';
 import { AttributesDirectivesComponent } from '../components/attributes-directives/attributes-directives.component';
@@ -7,7 +7,7 @@ import { AttributesDirectivesComponent } from '../components/attributes-directiv
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AttributesDirectivesComponent],
+  imports: [RouterOutlet, RouterLink, AttributesDirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AttributesDirectivesComponent } from '../components/attributes-directives/attributes-directives.component';
+import { DataBindingComponent } from '../components/data-binding/data-binding.component';
+import { StructuralDirectivesComponent } from '../components/structural-directives/structural-directives.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'attributes-directives',
+    component: AttributesDirectivesComponent,
+  },
+  {
+    path: 'data-binding',
+    component: DataBindingComponent,
+  },
+  {
+    path: 'structual-directives',
+    component: StructuralDirectivesComponent,
+  },
+];
