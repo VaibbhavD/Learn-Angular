f68c733a185eb60a0b4926f383bf9f10074e89ad
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-08-08 09:24:10 +0530

    add Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from '../components/data-binding/data-binding.component';
 import { StructuralDirectivesComponent } from '../components/structural-directives/structural-directives.component';
 import { AttributesDirectivesComponent } from '../components/attributes-directives/attributes-directives.component';
+import { RounterComponent } from '../components/rounter/rounter.component';
 
 @Component({
   selector: 'app-root',
--- src/components/rounter/rounter.component.ts
+++ src/components/rounter/rounter.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './rounter.component.html',
-  styleUrl: './rounter.component.css'
+  styleUrl: './rounter.component.css',
 })
-export class RounterComponent {
-
-}
+export class RounterComponent {}
